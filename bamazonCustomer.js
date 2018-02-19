9e01a75e2a9707abd4f2c50a5f328599f227dff1
Mike D <michael.mcdaniel210@gmail.com>
2018-02-18 21:40:43 -0600

    added buyorsell function and startup function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,9 +10,9 @@ var connection = mysql.createConnection({
   });
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+    // console.log("connected as id " + connection.threadId + "\n");
   });
-
+  startUp();
   function displayProducts (){
       console.log("");
       console.log("ITEMS CURRENTLY AVAILABLE ON BAMAZON:")
@@ -24,4 +24,32 @@ connection.connect(function(err) {
       });
 
   }
-  displayProducts();
+
+function startUp (){
+    console.log("");
+    console.log("Welcome to BAMAZON!  Here's a list of items available to purchase: ")
+    displayProducts();
+    buyOrSell();
+};
+
+function buyOrSell(){
+    inquirer
+        .prompt([
+            {
+                name: "buyorsell",
+                type: "checkbox",
+                message: "Would you like to Buy or Sell an item?",
+                choices: ["Buy an Item", "Sell an Item"]
+            },
+
+        ])
+        .then(function(res){
+             if(res.buyorsell === "Buy an Item"){
+                 console.log("It worked");
+                 return
+             }else{
+                 console.log("this also worked.")
+                 return
+             }
+        });
+};
