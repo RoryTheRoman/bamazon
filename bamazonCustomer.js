92889217654123c8f6e4834aa87749ea0efb8dd0
Mike D <michael.mcdaniel210@gmail.com>
2018-02-23 19:19:24 -0600

    app working, added bit to let a user post an item as well

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -99,41 +99,48 @@ function sellItem(){
 function buyItem (){
     connection.query("SELECT * FROM products", function(err, results) {
         if (err) throw err;
-        // console.log(results) shows that i'm getting results.;
+        // console.log(results) this shows that i'm getting results.;
         inquirer
         .prompt([
             {
                 type: "input",
                 name: "itemtobuy",
-                message: "What is the name of the item you would like to buy?",
+                message: "What is the item number of the product you'd like to buy?",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                      return true;
+                    }
+                    return false;
+                  }
             },
             {
                 type: "input",
                 name: "howmany",
-                message: "How many would you like to purchase?"
-                // validate: function(value){
-                //     if(isNaN(value) ===  false){
-                //         return true;
-                //     }
-                //     return false;
-                // }
-                //commented out, didn't like 'validate' as a thing in inquierer
-            }
+                message: "How many would you like to purchase?",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                      return true;
+                    }
+                    return false;
+                  }
+            },
         ])
         .then(function(answer){
             var chosenItem;
+            // console.log(results);   //i know i am getting results here, but for some reason it is not defining my chosen item.
             for (var i = 0; i < results.length; i ++){
-                if (results[i].item_name === answer.itemtobuy){
+                if (parseInt(results[i].item_id) === parseInt(answer.itemtobuy)){
+                    console.log(answer.itemtobuy);
                     chosenItem = results[i];
-                    console.log(chosenItem);
                 }
             }
-            if(parseInt(chosenItem.stock_quantity) > parseInt(answer.howmany)){
+
+            if(chosenItem.stock_quantity > answer.howmany){
                 connection.query(
                     "UPDATE products SET ? WHERE ?",
                     [
                         {
-                            stock_quantity: stock_quantity - answer.howmany
+                            stock_quantity: chosenItem.stock_quantity - answer.howmany
                         },
                         {
                             item_id: chosenItem.item_id
@@ -141,15 +148,20 @@ function buyItem (){
                     ],
                     function(error){
                         if (error) throw error;
-                        console.log("Congrats!  You are the proud owner of " + answer.howmany + " " + chosenItem + ".");
+                        console.log("");
+                        console.log("Congrats!  You are the proud owner of " + answer.howmany + " " + chosenItem.product_name + ".");
                     }
                 );
             }else{
+                console.log("");
                 console.log("I'm sorry, there aren't enough of those to buy. Try again!")
                 buyOrSell();
             }
         });
     });    
+}
+function goAgain(){
+
 }
 
 displayProducts();
